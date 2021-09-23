de15aebd979fb65c4805e1a3e029a4018cdedb24
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-23 22:37:32 +0530

    Quiz Finalized

---
+++ src/quiz.js
@@ -0,0 +1,22 @@
+const quizForm = document.querySelector('#Quiz-form');
+const quizBtn = document.querySelector('#Quiz-btn');
+const quizOutput = document.querySelector('#Quiz-output');
+
+const quizAnswers = ["90°", "Right Angled", "a + b + c", "85°"];
+
+function scoreCalculator() {
+    const quizResult = new FormData(quizForm);
+    let index = 0, score = 0;
+    for (let entry of quizResult.values()) {
+        if (entry === quizAnswers[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    quizOutput.innerText = "Your score is " + score;
+}
+
+quizBtn.addEventListener("click", scoreCalculator);
+
+
+
