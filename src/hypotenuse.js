7bab35a5378ccb01b33d4f660355e7f1170e1e6d
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-23 23:25:22 +0530

    Hypotenuse Finalized

---
+++ src/hypotenuse.js
@@ -0,0 +1,19 @@
+const lengthA = document.querySelector("#Length-A")
+const lengthB = document.querySelector("#Length-B")
+const calBtn = document.querySelector("#Length-Btn")
+const hypOutput = document.querySelector("#hyp-output")
+
+hypOutput.style.display = "none";
+
+// function hypCalculator() {
+//     var hyp = Math.sqrt((lengthA.value * lengthA.value) + (lengthB.value * lengthB.value))
+//     hypOutput.innerText = "the Hypotenuse is " + hyp + " units"
+//     hypOutput.style.display = "block";
+// }
+
+calBtn.addEventListener("click", function hypCalculator() {
+    var hyp = Math.sqrt((lengthA.value * lengthA.value) + (lengthB.value * lengthB.value))
+    hypOutput.innerText = "The Hypotenuse is " + hyp + " units."
+    hypOutput.style.display = "block";
+}
+);
