ade287fd1fa009f92a46e95301dd10c8c5c639ee
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2021-10-02 00:40:20 +0530

    minor logic fix (commit on "main")

--- src/Is-Triangle.js
+++ src/Is-Triangle.js
@@ -10,11 +10,14 @@ function sumOfAngles(angle1, angle2, angle3) {
 }
 function isTriangle() {
     const anglesSum = sumOfAngles(Number(angles[0].value), Number(angles[1].value), Number(angles[2].value));
-    if (anglesSum === 180) {
-        anglesOutput.innerText = "Wow, These angles do form a Triangle"
-    } else {
-        anglesOutput.innerText = "Oh oh, These angles do not form a Triangle."
-    }
+    if ((Number(angles[0].value)===0) || (Number(angles[1].value)===0) || (Number(angles[2].value)===0)) {
+        anglesOutput.innerText = "You need to specify all Three Values."
+    } else if (anglesSum === 180) {
+            anglesOutput.innerText = "Wow, These angles do form a Triangle"
+        } else {
+            anglesOutput.innerText = "Oh oh, These angles do not form a Triangle."
+        }
+    
 
 };
 
